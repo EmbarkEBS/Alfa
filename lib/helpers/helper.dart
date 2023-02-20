3613697cb0fbd64b2ab4569c1bb5f10cbcf32be3
EmbarkEBS <91586071+EmbarkEBS@users.noreply.github.com>
2023-02-20 22:33:11 +0530

    Removing old files

--- lib/helpers/helper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class Helper{
-  Future<bool> getAuth() async{
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.containsKey("user_type");
-  }
-}
