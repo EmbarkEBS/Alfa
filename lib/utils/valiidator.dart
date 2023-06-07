2258391415b390b6080e29713e523fb4a7378e9d
EmbarkEBS <ebs.sandbox@gmail.com>
2023-06-07 17:58:51 +0530

    changes lib and pubspec for splashscreen

---
+++ lib/utils/valiidator.dart
@@ -0,0 +1,11 @@
+class FieldValidator{
+  static String? validateEmail(String value){
+    if(value.isEmpty)return 'Enter Email!';
+    final RegExp regex =
+    RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)| (\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+    if (!regex.hasMatch(value))
+      return 'Enter a Valid Email!';
+    else
+      return null;
+  }
+}
